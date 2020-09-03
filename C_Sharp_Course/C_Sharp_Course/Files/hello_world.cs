25b077e394656ff0a4f32328363023cb32769fda
gmelektronik <david.stalder@gm-elektronik.swiss>
2020-09-03 08:33:09 +0200

    prints out "Hello World" to the console

---
+++ C_Sharp_Course/C_Sharp_Course/Files/hello_world.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();                     // Keeps console up until a line of code is entered
+        }
+    }
+}
