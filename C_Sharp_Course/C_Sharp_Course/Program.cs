be7c0a2e3fa13816565023514b5d7e2304407574
gmelektronik <david.stalder@gm-elektronik.swiss>
2020-09-04 11:08:05 +0200

    explains the two different types of while loops

--- C_Sharp_Course/C_Sharp_Course/Program.cs
+++ C_Sharp_Course/C_Sharp_Course/Program.cs
@@ -10,7 +10,27 @@ namespace C_Sharp_Course
     {
         static void Main(string[] args)
         {
-            
+            int index = 1;
+
+            while (index <= 5)
+            {
+                Console.WriteLine(index);
+                index++;
+            }
+
+            Console.WriteLine();
+
+            int index1 = 1;
+
+            do
+            {
+                Console.WriteLine(index1);
+                index1++;
+            } while (index1 <= 5);
+
+                Console.WriteLine();
+
+            Console.ReadLine();
         }
     }
 }
