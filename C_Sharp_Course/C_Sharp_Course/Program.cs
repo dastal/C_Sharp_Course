e588c799b7c9e7466fdf8ee3f51753328b4b464d
gmelektronik <david.stalder@gm-elektronik.swiss>
2020-09-04 15:59:19 +0200

    occasional commit before the weekend :)

--- C_Sharp_Course/C_Sharp_Course/Program.cs
+++ C_Sharp_Course/C_Sharp_Course/Program.cs
@@ -1,5 +1,4 @@
-﻿using C_Sharp_Course.Classes;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -12,18 +11,7 @@ namespace C_Sharp_Course
     {
         static void Main(string[] args)
         {
-            Movie avengers = new Movie("The Avengers", "Joss Whedon", "PG-13");
-            Movie shrek = new Movie("The Shrek", "Adma Adamson", "PG");
-
-            Console.WriteLine(avengers.Rating);
-
-            shrek.Rating = "Cat";
-
-            Console.WriteLine(shrek.Rating);
-
-            shrek.Rating = "R";
-
-            Console.WriteLine(shrek.Rating);
+            
 
             Console.WriteLine();
 
